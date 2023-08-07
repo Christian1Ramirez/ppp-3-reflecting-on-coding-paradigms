6e9e05b130421dfa272e50bcc077646fd031dc0a
Chrisitian J Ramirez <slipkmetall@yahoo.com>
2023-08-07 14:20:02 -0700

    first push

---
+++ main.py
@@ -0,0 +1,7 @@
+"""
+How does this solution ensure data immutability?
+Is this solution a pure function? Why or why not?
+Is this solution a higher order function? Why or why not?
+Would it have been easier to solve this problem using a different programming style?
+Why in particular is functional programming a helpful paradigm when solving this problem?
+"""
